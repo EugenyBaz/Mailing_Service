2b7d7642cf861c25c35aa91016492d37e37095ae
Bazavod <eugeny.bazavod@list.ru>
2025-06-23 22:59:37 +0300

    update button exit

--- users/urls.py
+++ users/urls.py
@@ -2,13 +2,13 @@ from django.contrib.auth.views import LoginView, LogoutView
 from users.apps import UsersConfig
 from django.urls import path
 
-from users.views import UserCreateView, email_verification, account_activation_sent
+from users.views import UserCreateView, email_verification, account_activation_sent, custom_logout
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", LogoutView.as_view(next_page='/users/login/'), name="logout"),
+    path("logout/", custom_logout, name="logout"),
     path("register/", UserCreateView.as_view(), name="register"),
     path("email-confirm/<str:token>", email_verification, name="email-confirm"),
     path('account_activation_sent/', account_activation_sent, name='account_activation_sent'),
---
+++ users/views.py
@@ -0,0 +1,51 @@
+import secrets
+
+from django.core.mail import send_mail
+from django.shortcuts import redirect, get_object_or_404, render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
+
+from users.forms import UserRegisterForm
+from users.models import User
+from config.settings import EMAIL_HOST_USER
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy("users:account_activation_sent")
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f"http://{host}/users/email-confirm/{token}"
+        send_mail(
+            subject="Подтверждение почты",
+            message=f"Привет, перейди по ссылке, для подтверждения почты. {url}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        return super().form_valid(form)
+
+def account_activation_sent(request):
+    return render(request, 'template_success_registration.html')
+
+
+def email_verification(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse("users:login"))
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+
+def custom_logout(request):
+    """Пользователь выходит из системы."""
+    logout(request)
+    messages.success(request, 'Вы успешно вышли.')
+    return HttpResponseRedirect('/users/login/')
